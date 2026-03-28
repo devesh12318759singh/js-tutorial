2e95df6fb3cafed98261d4617759ab0c20e5c316
Devesh Singh <152794584+devesh12318759singh@users.noreply.github.com>
2026-03-28 13:11:49 +0000

    conversions done

---
+++ js_tut/conversions.js
@@ -0,0 +1,13 @@
+let score = true
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33" -> 33
+//"33abc" -> NaN (but type gives number)
+
+

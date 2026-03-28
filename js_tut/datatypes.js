0675c28813c1d50d6bde6d5d18b518cef29932d5
Devesh Singh <152794584+devesh12318759singh@users.noreply.github.com>
2026-03-28 06:22:54 +0000

    second commit

---
+++ js_tut/datatypes.js
@@ -0,0 +1,6 @@
+"use strict"; // treat all JS code as newer version 
+let names = "Devesh"
+let age = 18
+let isloggedin = false
+
+console.table([names,age,isloggedin]);
---
+++ js_tut/variables.js
@@ -0,0 +1,15 @@
+const accntId = 144453
+let accEmail = "devesh@google.com"
+var accPass = "2438948"
+accCity = "Jaipur"
+let accountState;
+accEmail = "abc@jdkj.com"
+accPass = "3242434"
+accCity = "Etawah"
+
+console.log(accntId);
+
+/*
+Prefer not to use block scope and functional scope 
+*/
+console.table([accntId,accEmail,accPass,accCity,accountState]);

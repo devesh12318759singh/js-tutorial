7ae0b79dde285cfa458cc4d934c50ca338b1ff97
Devesh Singh <152794584+devesh12318759singh@users.noreply.github.com>
2026-03-28 19:32:13 +0000

    string half done

---
+++ js_tut/strings.js
@@ -0,0 +1,11 @@
+const name = "Devesh"
+const repocnt = 8
+
+//console.log(name + repocnt + " Value");
+
+//By using backticks : using placeholders
+console.log(`Hello my name is ${name} and my repo count is ${repocnt}`);
+
+//Some methods in string 
+console.log(name.charAt(3));
+console.log(name.indexOf('h'));

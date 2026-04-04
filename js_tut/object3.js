45343288a9df60054c69476d32df243c500756cb
Devesh Singh <152794584+devesh12318759singh@users.noreply.github.com>
2026-04-04 13:01:37 +0000

    object done with api understanding

---
+++ js_tut/object3.js
@@ -0,0 +1,12 @@
+const course = {
+    c_name: "Js in Hindi " ,
+    price: "39438" ,
+    instr: "Devesh"
+}
+
+//console.log(course.instr);
+
+//object destructuring 
+const {instr: ins} = course
+console.log(ins);
+

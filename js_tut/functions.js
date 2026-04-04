a34c92b5aa690eba9a7bdc473ba256241d43d94e
Devesh Singh <152794584+devesh12318759singh@users.noreply.github.com>
2026-04-04 13:20:48 +0000

    func done

---
+++ js_tut/functions.js
@@ -0,0 +1,24 @@
+/*
+function sayMyName(){
+    console.log("Devesh Singh");
+}
+
+sayMyName()
+*/
+
+function addnumbers(n1,n2){
+    console.log(n1+n2);
+}
+addnumbers(4,5);
+
+function addnumbers(n1,n2){
+    console.log(n1+n2);
+}
+addnumbers(4,"a");
+
+function addnumbers(n1,n2){
+    let result = n1+n2;
+    return result
+}
+const new_result = addnumbers(4,7);
+console.log("Result: ",new_result);

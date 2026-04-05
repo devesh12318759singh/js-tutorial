4e241abb54ff54323bc9ca9c5c3db6780ecd463d
Devesh Singh <152794584+devesh12318759singh@users.noreply.github.com>
2026-04-05 15:00:59 +0000

    done

---
+++ js_tut/func_obj.js
@@ -0,0 +1,17 @@
+function cartPrices(...num1){
+    return num1 
+}
+
+console.log(cartPrices(200,343,4540,20))
+
+//Object & function 
+const user = {
+    username: "Devesh" ,
+    amt: 343
+}
+
+function handleobj(anyob){
+    console.log(`Username is ${anyob.username} and amt is ${anyob.amt}`);
+
+}
+handleobj(user) 

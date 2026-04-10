b7ab3cda06ea0f1e40ad11cb5067203090da3c92
Devesh Singh <152794584+devesh12318759singh@users.noreply.github.com>
2026-04-10 14:37:50 +0000

    reduce done

--- second_step/filter_map.js
+++ second_step/filter_map.js
@@ -1,4 +1,25 @@
+/*
 const nums = [1,2,3,4,5,56,7,8]
+
 
 const new_n = nums.filter( (num) => num > 4)
 console.log(new_n);
+
+
+//Chaining concept 
+const newnums = nums.map( (num) => {return num*10}).map( (num) => num+1)
+
+console.log(newnums);
+*/
+
+
+//reduce 
+
+const numss = [1,2,3]
+
+const total = numss.reduce( function(acc,cur_val){
+    console.log(`Acc: ${acc} and cur_val: ${cur_val}`);
+    return acc+cur_val
+},0)
+
+console.log(total);
